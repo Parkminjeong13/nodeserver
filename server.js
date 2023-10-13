fabcafa03cb47fafac0a1293f36af0886d5a7a60
박민정 <ming1320@gmail.com>
2023-10-13 15:10:53 +0900

    231013 삭제 구현

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const dotenv = require('dotenv')
 
 dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 const {MongoClient, ObjectId} = require('mongodb');
@@ -59,6 +64,55 @@ app.get('/view/:id', async (req,res)=>{
     })
 })
 
+app.get('/write', (req,res)=>{
+    res.render('write.ejs')
+})
+
 app.get('/qna', (req,res)=>{
     res.send("qna 페이지2");
+})
+
+app.post('/add', async (req,res)=>{
+    console.log(req.body)
+    try{
+        await db.collection("notice").insertOne({
+            title: req.body.title,
+            content: req.body.content
+        })
+    }catch(error){
+        console.log(error)
+    }
+    // res.send("성공!")
+    res.redirect('/list')
+})
+
+app.put('/edit', async (req,res)=>{
+    console.log(req.body)
+    await db.collection("notice").updateOne({
+        _id : new ObjectId(req.body._id)
+    }, {
+        $set : {
+            title: req.body.title,
+            content: req.body.content
+        }
+    })
+    // const result = "";
+    // res.send(result)
+    res.redirect('/list')
+})
+
+app.get('/edit/:id', async (req,res)=>{
+    const result = await db.collection("notice").findOne({
+        _id : new ObjectId(req.params.id)
+    })
+    res.render('edit.ejs', {
+        data: result
+    })
+})
+
+app.delete('/delete', async (req,res)=>{
+    await db.collection("notice").deleteOne({
+        _id : new ObjectId(req.body._id)
+    })
+    res.redirect('/list')
 })
